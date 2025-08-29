858ec86fa74794bbcd5185658f127941469a0089
Gordon Situ <gsitu322@gmail.com>
2025-08-28 23:05:09 -0700

    Updating admin panel and rendering post from database

--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,17 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.caption}'
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
@@ -22,3 +31,6 @@ class Post(models.Model):
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name='posts', null=True)
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f'{self.title}'

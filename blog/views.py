a350a0c22e14bbbf545f6c744c6d263e51d2a1ff
Gordon Situ <gsitu322@gmail.com>
2025-08-29 00:18:50 -0700

    Rendering error and handling form submission

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.views import View
 from .forms import CommentForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 class StartingPageList(ListView):
     template_name = "blog/index.html"
@@ -20,13 +23,32 @@ class AllPosts(ListView):
     ordering = ["-date"]
     context_object_name = "all_posts"
 
-class PostDetail(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
-    context_object_name = "post"
+class PostDetail(View):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def get(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+
+        return render(request, "blog/post-detail.html", {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comments": post.comments.all().order_by("-id"),
+            "comment_form": CommentForm(),
+        })
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = get_object_or_404(Post, slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse("post_detail", args=[slug]))
+
+        return render(request, "blog/post-detail.html", {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comments": post.comments.all().order_by("-id"),
+            "comment_form": comment_form,
+        })

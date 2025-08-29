af208c1bab3817ac02f7cf76a65209ceb0f8e071
Gordon Situ <gsitu322@gmail.com>
2025-08-29 00:23:29 -0700

    Can view comments from admin panel

--- blog/models.py
+++ blog/models.py
@@ -32,10 +32,13 @@ class Post(models.Model):
     tags = models.ManyToManyField(Tag)
 
     def __str__(self):
-        return f'{self.title}'
+        return f"{self.title}"
 
 class Comment(models.Model):
     user_name = models.CharField(max_length=120)
     user_email = models.EmailField()
     text = models.TextField(max_length=400)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', null=True)
+
+    def __str__(self):
+        return f'{self.user_name}'
